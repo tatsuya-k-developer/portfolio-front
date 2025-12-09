1f654cf93dd53eef316779285ad3a72e418618ba
tatsuya-k-developer <tatsuya.developer.k@gmail.com>
2025-12-09 16:38:15 +0900

    環境構築

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,8 @@
-import Image from "next/image";
 
 export default function Page() {
   return (
-    <h1 className="text-3xl font-bold text-blue-600">
-      Hello Next.js!
-    </h1>
+    <main>
+
+    </main>
   );
 }
